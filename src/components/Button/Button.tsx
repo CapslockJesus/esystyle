65f1a77ae073d39c1959e61709a232f4d3617d1f
Daniel Reichert <capslockjesus@CapslockJesus.fritz.box>
2022-08-18 19:54:09 +0200

    tailwind init

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import './Button.css'
+import '../../main.css';
 import { ButtonProps } from './Button.types';
 
 const Button: FC<ButtonProps> = ({
@@ -10,12 +10,10 @@ const Button: FC<ButtonProps> = ({
         disabled
     }) => {
     return (
-        <button
-            className={`efy-button efy-button-${variant} efy-button-${buttonFunction}`}
-            onClick={onClick}
-            disabled={disabled}
-        >
-            {children}
+        <button className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
+            </svg>
         </button>
     );
 }
