324ada187289fcebbaa045bda5a72113da4df4fe
Daniel Reichert <capslockjesus@CapslockJesus.fritz.box>
2022-08-21 15:58:31 +0200

    Button Component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -59,8 +59,8 @@ export const Button: FC<ButtonProps> = (
         case 'import':
             return (
                 <button type={"button"} className={`${style} ${importStyle} ${disabled ? `${disabledStyle}` : ''}`} onClick={onClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 mr-2 ${iconVisible}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"/>
                     </svg>
                     <span>Importieren</span>
                 </button>
@@ -80,8 +80,7 @@ export const Button: FC<ButtonProps> = (
             return (
                 <button type={"button"} className={`${style} ${synchroStyle} ${disabled ? `${disabledStyle}` : ''}`} onClick={onClick}>
                     <svg xmlns="http://www.w3.org/2000/svg" className={`h-5 w-5 mr-2 ${iconVisible}`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"/>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"/>
                     </svg>
                     <span>Synchronisieren</span>
                 </button>
