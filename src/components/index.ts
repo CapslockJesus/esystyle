ade4fb78b0482c8358fcdf49974064e196dfe3f8
Daniel Reichert <capslockjesus@CapslockJesus.fritz.box>
2022-08-29 11:01:10 +0200

    Navbar Component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './Button';
 export { default as TextField } from './TextField';
 export { default as Label } from './Label';
 export { default as TextArea } from './TextArea';
+export { default as Navbar } from './Navbar';
