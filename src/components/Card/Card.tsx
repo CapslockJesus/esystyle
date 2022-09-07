fcfc0c15acdd35a9084242eaeee5b0d8b2e0d7f7
Daniel Reichert <capslockjesus@CapslockJesus.fritz.box>
2022-09-07 12:09:49 +0200

    Card angepasst

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,7 +7,7 @@ export const Card: FC<ComponentInterface> = ({name, onClick, id}) => {
 
     return (
         <div
-            className={`flex w-60 h-fit ml-2 pl-16 pr-16 pt-10 pb-10 bg-yellow-300 hover:bg-yellow-400 hover:cursor-pointer ease-in-out duration-100 ease-in hover:shadow hover:shadow-zinc-200 justify-center rounded-lg`}
+            className={`flex w-full h-fit mb-2 pl-16 pr-16 pt-3 pb-3 bg-yellow-200 hover:bg-yellow-400 hover:cursor-pointer ease-in-out duration-100 ease-in hover:shadow hover:shadow-zinc-200 justify-center rounded-lg`}
             id={id}
             onClick={onClick}
         >
